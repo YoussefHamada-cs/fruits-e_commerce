14383ab6f7c6a67271458e5e4cccb94f8a9fa64c
youssef hamada <yosefhagag84171@gmail.com>
2025-09-04 17:06:05 +0300

    start app

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FruitsHub());
+}
+
+class FruitsHub extends StatelessWidget {
+  const FruitsHub({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     
+    );
+  }
+}
