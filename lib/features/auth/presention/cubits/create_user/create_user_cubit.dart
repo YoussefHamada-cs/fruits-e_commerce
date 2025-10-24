69d7667b0340a42efb2cb74a59dcc84b1f64dd4e
youssef hamada <yosefhagag84171@gmail.com>
2025-10-25 02:41:16 +0300

    finsh cubit create user

---
+++ lib/features/auth/presention/cubits/create_user/create_user_cubit.dart
@@ -0,0 +1,28 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruits_hub/features/auth/domain/entites/user_entity.dart';
+import 'package:fruits_hub/features/auth/domain/repos/auth_repo.dart';
+
+
+part 'create_user_state.dart';
+
+class CreateUserCubit extends Cubit<CreateUserState> {
+  CreateUserCubit(this.authRepo) : super(CreateUserInitial());
+   final AuthRepo authRepo;
+
+  Future<void> createUserWithEmailAndPassword(
+      String email, String password, String name) async {
+    emit(CreateUserLoading());
+    final result =
+        await authRepo.createUserWithEmailAndPassword(email, password, name);
+    result.fold(
+      (failure) => emit(
+       CreateUserFailure(message: failure.message!),
+      ),
+      (userEntity) => emit(
+        CreateUserSuccess(userEntity: userEntity),
+      ),
+    );
+  }
+}
