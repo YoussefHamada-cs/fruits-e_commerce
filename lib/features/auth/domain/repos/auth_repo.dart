ca58d216e98d10da4bdb2ffe7424516d4198a8f0
youssef hamada <yosefhagag84171@gmail.com>
2025-10-25 02:33:41 +0300

    add functions createuser and login in repo

---
+++ lib/features/auth/domain/repos/auth_repo.dart
@@ -0,0 +1,14 @@
+// lib/feature/auth/domain/repositories/auth_repo.dart
+import 'package:dartz/dartz.dart';
+import 'package:fruits_hub/core/errors/failures.dart';
+import 'package:fruits_hub/features/auth/domain/entites/user_entity.dart';
+
+
+abstract class AuthRepo {
+  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword(String email,String  password,String name);
+  Future<Either<Failure, UserEntity>> loginWithEmailAndPassword(String email,String  password);
+  Future<Either<Failure, UserEntity>> loginWithGoogle();
+  Future<Either<Failure, UserEntity>> loginWithFacebook();
+  Future<Either<Failure, UserEntity>> loginWithApple();
+ 
+}
