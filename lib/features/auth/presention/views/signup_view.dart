cc28b6168870dbc983ab4a31cc8f45b74a37ab56
youssef hamada <yosefhagag84171@gmail.com>
2025-10-25 23:13:30 +0300

    add blocprovider createUserCubit in SignupView

--- lib/features/auth/presention/views/signup_view.dart
+++ lib/features/auth/presention/views/signup_view.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruits_hub/core/service/get_it_sevice.dart';
 import 'package:fruits_hub/core/utils/app_strings.dart';
 import 'package:fruits_hub/core/widgets/custom_app_bar.dart';
+import 'package:fruits_hub/features/auth/domain/repos/auth_repo.dart';
+import 'package:fruits_hub/features/auth/presention/cubits/create_user/create_user_cubit.dart';
 import 'package:fruits_hub/features/auth/presention/views/widgets/signup_view_body.dart';
 
 class SignupView extends StatelessWidget {
@@ -10,7 +14,12 @@ class SignupView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: customAppBar(title: AppStrings.signUp, context),
-      body: SignupViewBody()
+      body: BlocProvider(
+        create: (context) => CreateUserCubit(
+        getIt<AuthRepo>() 
+        ),
+        child: SignupViewBody(),
+      )
     
     );
   }
