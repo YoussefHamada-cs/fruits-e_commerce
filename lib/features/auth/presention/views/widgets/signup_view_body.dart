e5144cf822b07b272363cbd01183bdfc0276aa76
youssef hamada <yosefhagag84171@gmail.com>
2025-10-26 14:06:04 +0300

    custom snackbar

--- lib/features/auth/presention/views/widgets/signup_view_body.dart
+++ lib/features/auth/presention/views/widgets/signup_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruits_hub/core/helper/custom_show_snack_bar.dart';
 import 'package:fruits_hub/features/auth/presention/cubits/create_user/create_user_cubit.dart';
 import 'package:fruits_hub/features/auth/presention/views/widgets/signup_form.dart';
 
@@ -12,10 +13,9 @@ class SignupViewBody extends StatelessWidget {
       listener: (context, state) {
         if (state is CreateUserSuccess) {
           Navigator.of(context).pop();
+          customShowSnackBar(context, 'تم إنشاء الحساب بنجاح');
         } else if (state is CreateUserFailure) {
-          ScaffoldMessenger.of(
-            context,
-          ).showSnackBar(SnackBar(content: Text(state.message)));
+          customShowSnackBar(context, state.message);
         }
       },
       builder: (context, state) {
@@ -25,4 +25,5 @@ class SignupViewBody extends StatelessWidget {
       },
     );
   }
+
 }
