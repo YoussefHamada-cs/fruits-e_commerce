975e9d6ef1c1a6f2199ff5b8972ef2cd2982b923
youssef hamada <yosefhagag84171@gmail.com>
2025-10-26 16:56:01 +0300

    add snackbar in CreateUserSuccess

--- lib/features/auth/presention/views/widgets/signup_view_body.dart
+++ lib/features/auth/presention/views/widgets/signup_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruits_hub/core/helper/custom_show_snack_bar.dart';
+import 'package:fruits_hub/core/utils/app_strings.dart';
 import 'package:fruits_hub/features/auth/presention/cubits/create_user/create_user_cubit.dart';
 import 'package:fruits_hub/features/auth/presention/views/widgets/signup_form.dart';
 
@@ -13,7 +14,7 @@ class SignupViewBody extends StatelessWidget {
       listener: (context, state) {
         if (state is CreateUserSuccess) {
           Navigator.of(context).pop();
-          customShowSnackBar(context, 'تم إنشاء الحساب بنجاح');
+          customShowSnackBar(context, AppStrings.accountCreatedSuccessfully);
         } else if (state is CreateUserFailure) {
           customShowSnackBar(context, state.message);
         }
