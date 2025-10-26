000b62531f50b46fc5239932805598f06ad22a69
youssef hamada <yosefhagag84171@gmail.com>
2025-10-26 23:07:20 +0300

    create cubit login and finsh loginWithEmailAndPassword

---
+++ lib/features/auth/presention/cubits/login/login_cubit.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruits_hub/features/auth/domain/entites/user_entity.dart';
+import 'package:fruits_hub/features/auth/domain/repos/auth_repo.dart';
+
+part 'login_state.dart';
+
+class LoginCubit extends Cubit<LoginState> {
+  final AuthRepo authRepo;
+
+  LoginCubit({required this.authRepo}) : super(LoginInitial());
+
+  Future<void> loginWithEmailAndPassword(String email, String password) async {
+    emit(LoginLoading());
+    var result = await authRepo.loginWithEmailAndPassword(email, password);
+    result.fold(
+      (failure) => emit(
+       LoginFailure(message: failure.message!),
+      ),
+      (userEntity) => emit(
+        LoginSuccess(user: userEntity),
+      ),
+    );
+  }
+}
