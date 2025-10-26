37f0dc97f418605b981905a882f011685325a583
youssef hamada <yosefhagag84171@gmail.com>
2025-10-26 23:07:41 +0300

    custom indictoer

---
+++ lib/core/widgets/custom_indictor.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/material.dart';
+import 'package:fruits_hub/core/utils/app_colors.dart';
+
+class CustomIndicator extends StatelessWidget {
+  const CustomIndicator({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        height: 25,
+        width: 25,
+        child: const CircularProgressIndicator(
+          color: AppColors.green,
+        ),
+      );
+  }
+}
