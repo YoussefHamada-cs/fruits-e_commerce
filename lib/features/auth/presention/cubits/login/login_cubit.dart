2d49c1f9caced63129d6a33e597776b2d56f1271
youssef hamada <yosefhagag84171@gmail.com>
2025-10-27 11:10:21 +0300

    add loginWithGoogle in loginCubit

--- lib/features/auth/presention/cubits/login/login_cubit.dart
+++ lib/features/auth/presention/cubits/login/login_cubit.dart
@@ -22,4 +22,17 @@ class LoginCubit extends Cubit<LoginState> {
       ),
     );
   }
+  Future<void> loginWithGoogle() async {
+
+    emit(LoginLoading());
+    final result = await authRepo.loginWithGoogle();
+    result.fold(
+      (failure) => emit(
+       LoginFailure(message: failure.message!),
+      ),
+      (userEntity) => emit(
+        LoginSuccess(user: userEntity),
+      ),
+    );
+  }
 }
