37db907d9a4e695a1c5ce4bd986e7792df4f3a2b
youssef hamada <yosefhagag84171@gmail.com>
2025-10-28 20:55:06 +0300

    add databaseservice in getitService

--- lib/core/service/get_it_sevice.dart
+++ lib/core/service/get_it_sevice.dart
@@ -1,3 +1,5 @@
+import 'package:fruits_hub/core/service/data_base_service.dart';
+import 'package:fruits_hub/core/service/fire_store_service.dart';
 import 'package:fruits_hub/core/service/firebase_auth_service.dart';
 import 'package:fruits_hub/features/auth/data/repos/auth_repo_impl.dart';
 import 'package:fruits_hub/features/auth/domain/repos/auth_repo.dart';
@@ -9,8 +11,11 @@ void setupGetIt() {
     getIt.registerSingleton<FirebaseAuthService>(
       FirebaseAuthService()
   );
+    getIt.registerSingleton<DataBaseService>(
+      FireStoreService()
+  );
   getIt.registerSingleton<AuthRepo>(AuthRepoImpl(
-    firebaseAuthService: getIt<FirebaseAuthService>()
+    firebaseAuthService: getIt<FirebaseAuthService>(), dataBaseService: getIt<DataBaseService>()
   ));
 
 
