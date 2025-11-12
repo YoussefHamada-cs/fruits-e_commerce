2223d9be9bf464dfbf6d65cb6375112c311c4a7d
youssef hamada <yosefhagag84171@gmail.com>
2025-11-12 10:37:25 +0200

    delete image from entity

---
+++ lib/core/entities/product_entity.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+import 'package:fruits_hub/core/entities/review_entity.dart';
+
+
+
+class ProductEntity {
+  final String name;
+  final String code;
+  final String description;
+  final num price;
+  final bool isFeatured;
+  String? imageUrl;
+  final int expirationsMonths;
+  final bool isOrganic;
+  final int numberOfCalories;
+  final num avgRating = 0;
+  final num ratingCount = 0;
+  final int unitAmount;
+  final List<ReviewEntity>reviewsModel;
+  ProductEntity({
+    this.imageUrl,
+    required this.name,
+    required this.code,
+    required this.description,
+    required this.price,
+    required this.expirationsMonths,
+    required this.isOrganic,
+    required this.numberOfCalories,
+    required this.unitAmount,
+    required this.isFeatured,
+    required this.reviewsModel,
+  });
+}
