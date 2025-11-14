d81731e378d3d5e60b022b04e44d9184645993a2
youssef hamada <yosefhagag84171@gmail.com>
2025-11-14 12:33:20 +0200

    finsh ProductsCubit

---
+++ lib/core/cubits/products_cubit/products_cubit.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruits_hub/core/entities/product_entity.dart';
+import 'package:fruits_hub/core/repos/product_repo.dart';
+
+
+
+part 'products_state.dart';
+
+class ProductsCubit extends Cubit<ProductsState> {
+  ProductsCubit(this.productsRepo) : super(ProductsInitial());
+
+  final ProductRepo productsRepo;
+  int productsLength = 0;
+  Future<void> getProducts() async {
+    emit(ProductsLoading());
+    final result = await productsRepo.getProducts();
+    result.fold(
+      (failure) => emit(ProductsFailure(failure.message)),
+      (products) => emit(ProductsSuccess(products)),
+    );
+  }
+
+  Future<void> getBestSellingroducts() async {
+    emit(ProductsLoading());
+    final result = await productsRepo.getBestSellingProducts();
+    result.fold(
+      (failure) => emit(ProductsFailure(failure.message)),
+      (products) {
+        productsLength = products.length;
+        emit(ProductsSuccess(products));
+      },
+    );
+  }
+}
