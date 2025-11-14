8158c83ab69fd4f47bb7af358c8852937d2a12dd
youssef hamada <yosefhagag84171@gmail.com>
2025-11-14 17:29:29 +0200

    add blocprovider to homeview

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruits_hub/core/cubits/products_cubit/products_cubit.dart';
+import 'package:fruits_hub/core/repos/product_repo.dart';
+import 'package:fruits_hub/core/service/get_it_sevice.dart';
 import 'package:fruits_hub/features/home/presentation/views/widgets/home_view_body.dart';
 
 class HomeView extends StatelessWidget {
@@ -10,7 +14,12 @@ class HomeView extends StatelessWidget {
       body: SafeArea(
         child: Directionality(
           textDirection: TextDirection.rtl,
-          child: const HomeViewBody(),
+          child: BlocProvider(
+            create: (context) => ProductsCubit(
+              getIt<ProductRepo>()
+            ),
+            child: const HomeViewBody(),
+          ),
         ),
       ),
     );
