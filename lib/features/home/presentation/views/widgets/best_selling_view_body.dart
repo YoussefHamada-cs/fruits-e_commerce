ca347b8390102ccb41f94bbfabe470320665b09d
youssef hamada <yosefhagag84171@gmail.com>
2025-11-14 17:31:10 +0200

    read ProductsCubit in BestSellingViewBody

--- lib/features/home/presentation/views/widgets/best_selling_view_body.dart
+++ lib/features/home/presentation/views/widgets/best_selling_view_body.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruits_hub/core/cubits/products_cubit/products_cubit.dart';
 import 'package:fruits_hub/core/utils/app_strings.dart';
-import 'package:fruits_hub/features/home/presentation/views/widgets/best_selling_grid_view.dart';
+import 'package:fruits_hub/core/presentation/widgets/products_grid_view_bloc_builder.dart';
 
-class BestSellingViewBody extends StatelessWidget {
+class BestSellingViewBody extends StatefulWidget {
   const BestSellingViewBody({super.key});
 
+  @override
+  State<BestSellingViewBody> createState() => _BestSellingViewBodyState();
+}
+
+class _BestSellingViewBodyState extends State<BestSellingViewBody> {
+   @override
+  void initState() {
+    context.read<ProductsCubit>().getBestSellingroducts();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
@@ -16,7 +28,7 @@ class BestSellingViewBody extends StatelessWidget {
             child: Text(AppStrings.bestSelling, style: textTheme.titleSmall),
           ),
           SliverToBoxAdapter(child: SizedBox(height: 12)),
-          BestSellingGridView(),
+          ProductsGridViewBlocBuilder(),
         ],
       ),
     );
