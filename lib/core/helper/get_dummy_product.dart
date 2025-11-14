6eddaf48a6b7776c300e0db604a2a4fe26753cb3
youssef hamada <yosefhagag84171@gmail.com>
2025-11-14 14:06:24 +0200

    getDummyProduct

---
+++ lib/core/helper/get_dummy_product.dart
@@ -0,0 +1,30 @@
+
+
+import 'package:fruits_hub/core/entities/product_entity.dart';
+
+ProductEntity getDummyProduct() {
+  
+  return ProductEntity(
+    name: 'Apple',
+    code: '123',
+    description: 'Fresh apple',
+    price: 2.5,
+    reviewsModel: [],
+    expirationsMonths: 6,
+    numberOfCalories: 100,
+    unitAmount: 1,
+    isOrganic: true,
+    isFeatured: true,
+    imageUrl: null,
+  );
+}
+
+List<ProductEntity> getDummyProducts() {
+  return [
+    getDummyProduct(),
+    getDummyProduct(),
+    getDummyProduct(),
+    getDummyProduct(),
+    getDummyProduct(),
+  ];
+}
