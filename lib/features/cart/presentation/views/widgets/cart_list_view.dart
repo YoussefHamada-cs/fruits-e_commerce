dfd5ea818f6b58ba9c0cc553e980c660f882bf0d
youssef hamada <yosefhagag84171@gmail.com>
2025-11-21 11:58:19 +0200

    add logic for CartListView

--- lib/features/cart/presentation/views/widgets/cart_list_view.dart
+++ lib/features/cart/presentation/views/widgets/cart_list_view.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/features/cart/domain/entites/car_item_entity.dart';
 import 'package:fruits_hub/features/cart/presentation/views/widgets/cart_item.dart';
 import 'package:fruits_hub/features/cart/presentation/views/widgets/custom_divider.dart';
 
 class CartListView extends StatelessWidget {
-  const CartListView({super.key});
-
+  const CartListView({super.key, required this.cartList});
+  final List<CartItemEntity> cartList;
   @override
   Widget build(BuildContext context) {
     return ListView.separated(
-      itemBuilder: (context, index) => CartItem(),
+      itemBuilder: (context, index) =>
+          CartItem(cartItemEntity: cartList[index]),
       physics: NeverScrollableScrollPhysics(),
       shrinkWrap: false,
       separatorBuilder: (context, index) => CustomDivider(),
-      itemCount: 10,
+      itemCount: cartList.length,
     );
   }
 }
