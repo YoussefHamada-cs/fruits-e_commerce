de79331126ead56e6c09e332bd00de20dbb5227a
youssef hamada <yosefhagag84171@gmail.com>
2025-11-21 11:52:12 +0200

    add logic ActionButtonsRemoveAndAdd

--- lib/features/cart/presentation/views/widgets/action_buttons_remove_and_add.dart
+++ lib/features/cart/presentation/views/widgets/action_buttons_remove_and_add.dart
@@ -1,28 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruits_hub/core/utils/app_colors.dart';
+import 'package:fruits_hub/features/cart/domain/entites/car_item_entity.dart';
+import 'package:fruits_hub/features/cart/presentation/views/cubits/cart_item_cubit/cart_item_cubit.dart';
 
 class ActionButtonsRemoveAndAdd extends StatelessWidget {
-  const ActionButtonsRemoveAndAdd({super.key});
-
+  const ActionButtonsRemoveAndAdd({super.key, required this.cartItemEntity});
+  final CartItemEntity cartItemEntity;
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
     return Row(
       children: [
-        CircleAvatar(
-          radius: 14,
+        GestureDetector(
+          onTap: () {
+            cartItemEntity.increasQuantity();
+            context.read<CartItemCubit>().updateCartItem(cartItemEntity);
+          },
+          child: CircleAvatar(
+            radius: 14,
 
-          backgroundColor: AppColors.green,
-          child: Icon(Icons.add, color: Colors.white),
+            backgroundColor: AppColors.green,
+            child: Icon(Icons.add, color: Colors.white),
+          ),
         ),
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Text('3', style: textTheme.titleMedium),
+          child: Text(
+            cartItemEntity.quanitty.toString(),
+            style: textTheme.titleMedium,
+          ),
         ),
-        CircleAvatar(
-          backgroundColor: AppColors.backgroundCard,
-          radius: 14,
-          child: Icon(Icons.remove, color: Colors.grey),
+        GestureDetector(
+          onTap: () {
+            cartItemEntity.decreasQuantity();
+            context.read<CartItemCubit>().updateCartItem(cartItemEntity);
+          },
+          child: CircleAvatar(
+            backgroundColor: AppColors.backgroundCard,
+            radius: 14,
+            child: Icon(Icons.remove, color: Colors.grey),
+          ),
         ),
       ],
     );
