db314473613596c3ce8240eb1b4b80423e52594f
youssef hamada <yosefhagag84171@gmail.com>
2025-11-21 21:38:47 +0200

    Add appbar in checkoutview

--- lib/features/checkout/presentation/views/checkout_view.dart
+++ lib/features/checkout/presentation/views/checkout_view.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/core/presentation/widgets/custom_app_bar.dart';
 import 'package:fruits_hub/features/checkout/presentation/views/widgets/checkout_view_body.dart';
+
 class CheckoutView extends StatelessWidget {
   const CheckoutView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CheckoutViewBody() ,
-    );}}
+      appBar: customAppBar(context, title: 'الشحن', onPressed: () {}),
+      body: CheckoutViewBody(),
+    );
+  }
+}
