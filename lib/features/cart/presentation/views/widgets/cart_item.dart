3dd100e4e7ecdb6f0326909a39332195bfa427fb
youssef hamada <yosefhagag84171@gmail.com>
2025-11-21 11:57:07 +0200

    display data and rebuild for one item in CartItem

--- lib/features/cart/presentation/views/widgets/cart_item.dart
+++ lib/features/cart/presentation/views/widgets/cart_item.dart
@@ -1,62 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:fruits_hub/core/presentation/widgets/custom_image_network.dart';
 import 'package:fruits_hub/core/utils/app_colors.dart';
 import 'package:fruits_hub/core/utils/app_images.dart';
+import 'package:fruits_hub/features/cart/domain/entites/car_item_entity.dart';
+import 'package:fruits_hub/features/cart/presentation/views/cubits/cart_cubit/cart_cubit.dart';
+import 'package:fruits_hub/features/cart/presentation/views/cubits/cart_item_cubit/cart_item_cubit.dart';
 import 'package:fruits_hub/features/cart/presentation/views/widgets/action_buttons_remove_and_add.dart';
 
 class CartItem extends StatelessWidget {
-  const CartItem({super.key});
-
+  const CartItem({super.key, required this.cartItemEntity});
+  final CartItemEntity cartItemEntity;
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
-    return IntrinsicHeight(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 12.0),
-        child: Row(
-          children: [
-            Container(
-              width: 73,
-              height: 92,
-              color: AppColors.backgroundCard,
-              child: Center(child: Image.asset(AppImages.watermelonTest)),
-            ),
-            const SizedBox(width: 17),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+    return BlocBuilder<CartItemCubit, CartItemState>(
+      buildWhen: (prev, current) {
+        if (current is CartItemUpdated) {
+          if (current.cartItemEntity == cartItemEntity) {
+            return true;
+          }
+        }
+        return false;
+      },
+      builder: (context, state) {
+        return IntrinsicHeight(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12.0),
+            child: Row(
               children: [
-                Text('بطيخ', style: textTheme.displayMedium),
-                SizedBox(height: 10),
-                Text(
-                  '3كيلو',
-                  style: textTheme.bodyMedium!.copyWith(
-                    color: AppColors.colorUnitPrice,
+                Container(
+                  width: 73,
+                  height: 92,
+                  color: AppColors.backgroundCard,
+                  child: Center(
+                    child: CustomImageNetwork(
+                      imageUrl: cartItemEntity.productEntity.imageUrl,
+                    ),
                   ),
                 ),
-                SizedBox(height: 15),
-                ActionButtonsRemoveAndAdd(),
+                const SizedBox(width: 17),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      cartItemEntity.productEntity.name,
+                      style: textTheme.displayMedium,
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      '${cartItemEntity.calculateTotalWeight()}كيلو',
+                      style: textTheme.bodyMedium!.copyWith(
+                        color: AppColors.colorUnitPrice,
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                    ActionButtonsRemoveAndAdd(cartItemEntity: cartItemEntity),
+                  ],
+                ),
+                Spacer(),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        context.read<CartCubit>().deleteCarItem(cartItemEntity);
+                      },
+                      child: SvgPicture.asset(AppImages.trash),
+                    ),
+                    SizedBox(height: 40),
+                    Text(
+                      '${cartItemEntity.calculateTotalPrice()} جنيه',
+                      style: textTheme.titleMedium!.copyWith(
+                        color: AppColors.colorPrice,
+                      ),
+                    ),
+                  ],
+                ),
               ],
             ),
-            Spacer(),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                GestureDetector(
-                  onTap: () {},
-                  child: SvgPicture.asset(AppImages.trash),
-                ),
-                SizedBox(height: 40),
-                Text(
-                  '60 جنيه',
-                  style: textTheme.titleMedium!.copyWith(
-                    color: AppColors.colorPrice,
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
