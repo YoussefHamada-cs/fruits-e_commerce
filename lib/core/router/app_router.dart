f249c7fa21528cb09fc430f7ad78b48976179957
youssef hamada <yosefhagag84171@gmail.com>
2025-11-21 11:46:18 +0200

    CartCubitWrapper in Approuter

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/core/presentation/main_views/cart_cubit_wrapper.dart';
 import 'package:fruits_hub/core/presentation/main_views/main_views.dart';
 import 'package:fruits_hub/core/router/app_routes.dart';
 import 'package:fruits_hub/core/router/app_transitions.dart';
@@ -54,90 +55,85 @@ class AppRouter {
           child: const SignupView(),
         ),
       ),
-// ----------------------- Authenticated routes (with bottom nav) -----------------------
-StatefulShellRoute.indexedStack(
-  builder: (context, state, navigationShell) =>
-      MainViews(shell: navigationShell),
+      // ----------------------- Authenticated routes (with bottom nav) -----------------------
+      StatefulShellRoute.indexedStack(
+        builder: (context, state, navigationShell) =>
+            CartCubitWrapper(child: MainViews(shell: navigationShell)),
 
-  branches: [
-
-    // ---------------- HOME ----------------
-    StatefulShellBranch(
-      routes: [
-        GoRoute(
-          path: AppRoutes.home,
-          name: AppRoutes.home,
-          pageBuilder: (context, state) => AppTransitions.size(
-            context: context,
-            state: state,
-            child: HomeView(),
-          ),
-          routes: [
-            GoRoute(
-              path: AppRoutes.bestSelling,
-              name: AppRoutes.bestSelling,
-              pageBuilder: (context, state) => AppTransitions.size(
-                context: context,
-                state: state,
-                child: const BestSellingView(),
+        branches: [
+          // ---------------- HOME ----------------
+          StatefulShellBranch(
+            routes: [
+              GoRoute(
+                path: AppRoutes.home,
+                name: AppRoutes.home,
+                pageBuilder: (context, state) => AppTransitions.size(
+                  context: context,
+                  state: state,
+                  child: HomeView(),
+                ),
+                routes: [
+                  GoRoute(
+                    path: AppRoutes.bestSelling,
+                    name: AppRoutes.bestSelling,
+                    pageBuilder: (context, state) => AppTransitions.size(
+                      context: context,
+                      state: state,
+                      child: const BestSellingView(),
+                    ),
+                  ),
+                ],
               ),
-            ),
-          ],
-        ),
-      ],
-    ),
-
-   
-  
-
-    // ---------------- PRODUCTS ----------------
-    StatefulShellBranch(
-      routes: [
-        GoRoute(
-          path: AppRoutes.products,
-          name: AppRoutes.products,
-          pageBuilder: (context, state) => AppTransitions.size(
-            context: context,
-            state: state,
-            child: const ProductsView(),
+            ],
           ),
-        ),
-      ],
-    ),
-     // ---------------- CART ----------------
-      StatefulShellBranch(
-      routes: [
-        GoRoute(
-          path: AppRoutes.cart,
-          name: AppRoutes.cart,
-          pageBuilder: (context, state) => AppTransitions.size(
-            context: context,
-            state: state,
-            child:CartView(),
-          ),
-        ),
-      ],
-    ),
 
-    // ---------------- PROFILE ----------------
-    StatefulShellBranch(
-      routes: [
-        GoRoute(
-          path: AppRoutes.profile,
-          name: AppRoutes.profile,
-          pageBuilder: (context, state) => AppTransitions.size(
-            context: context,
-            state: state,
-            child: const Scaffold(
-              body: Center(child: Text('Profile View')),
-            ),
+          // ---------------- PRODUCTS ----------------
+          StatefulShellBranch(
+            routes: [
+              GoRoute(
+                path: AppRoutes.products,
+                name: AppRoutes.products,
+                pageBuilder: (context, state) => AppTransitions.size(
+                  context: context,
+                  state: state,
+                  child: const ProductsView(),
+                ),
+              ),
+            ],
           ),
-        ),
-      ],
-    ),
-  ],
-),
+          // ---------------- CART ----------------
+          StatefulShellBranch(
+            routes: [
+              GoRoute(
+                path: AppRoutes.cart,
+                name: AppRoutes.cart,
+                pageBuilder: (context, state) => AppTransitions.size(
+                  context: context,
+                  state: state,
+                  child: CartView(),
+                ),
+              ),
+            ],
+          ),
 
+          // ---------------- PROFILE ----------------
+          StatefulShellBranch(
+            routes: [
+              GoRoute(
+                path: AppRoutes.profile,
+                name: AppRoutes.profile,
+                pageBuilder: (context, state) => AppTransitions.size(
+                  context: context,
+                  state: state,
+                  child: const Scaffold(
+                    body: Center(child: Text('Profile View')),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     ],
   );
 }
