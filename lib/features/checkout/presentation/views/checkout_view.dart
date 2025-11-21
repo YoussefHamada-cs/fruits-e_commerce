6d274e89a1e1786c62e8e8447d3fbd75f01adf1e
youssef hamada <yosefhagag84171@gmail.com>
2025-11-21 22:46:23 +0200

    display name step in appbar

--- lib/features/checkout/presentation/views/checkout_view.dart
+++ lib/features/checkout/presentation/views/checkout_view.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:fruits_hub/core/presentation/widgets/custom_app_bar.dart';
+import 'package:fruits_hub/features/checkout/presentation/views/widgets/checkout_steps.dart';
 import 'package:fruits_hub/features/checkout/presentation/views/widgets/checkout_view_body.dart';
 
-class CheckoutView extends StatelessWidget {
+class CheckoutView extends StatefulWidget {
   const CheckoutView({super.key});
+
+  @override
+  State<CheckoutView> createState() => _CheckoutViewState();
+}
+
+class _CheckoutViewState extends State<CheckoutView> {
+  int currentStep = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: customAppBar(context, title: 'الشحن', onPressed: () {}),
-      body: CheckoutViewBody(),
+      appBar: customAppBar(
+        context,
+        title: getSteps()[currentStep],
+        onPressed: () {},
+      ),
+      body: CheckoutViewBody(
+        onStepChanged: (index) {
+          setState(() {
+            currentStep = index;
+          });
+        },
+      ),
     );
   }
 }
