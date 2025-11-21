906dbedf72a931852e681f6c4e5111e84eb8f6d2
youssef hamada <yosefhagag84171@gmail.com>
2025-11-21 11:58:41 +0200

    CustomCartButton

---
+++ lib/features/cart/presentation/views/widgets/custom_cart_button.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruits_hub/core/presentation/widgets/custom_button.dart';
+import 'package:fruits_hub/features/cart/presentation/views/cubits/cart_cubit/cart_cubit.dart';
+import 'package:fruits_hub/features/cart/presentation/views/cubits/cart_item_cubit/cart_item_cubit.dart';
+
+class CustomCartButton extends StatelessWidget {
+  const CustomCartButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<CartItemCubit, CartItemState>(
+      builder: (context, state) {
+        return CustomButton(
+          text:
+              ' الدفع ${context.watch<CartCubit>().cartEntity.calculateTotalPrice()} جنيه',
+          onPressed: () {},
+        );
+      },
+    );
+  }
+}
