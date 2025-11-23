9405dfd6c15b5cedf466dc89ffe157240ee5c47e
youssef hamada <yosefhagag84171@gmail.com>
2025-11-23 09:45:04 +0200

    ShippingAddressEntity

---
+++ lib/features/checkout/domain/entites/shipping_address_entity.dart
@@ -0,0 +1,10 @@
+class ShippingAddressEntity {
+  final String name;
+  final String email;
+  final String phone;
+  final String address;
+  final String city;
+final String addressDetails;
+
+  ShippingAddressEntity({required this.name, required this.email, required this.phone, required this.address, required this.city, required this.addressDetails});
+}
